31883c85e10eb62675f1339b445fccff8a40d719
Nitin <nsinghboss02@gmail.com>
2022-01-28 17:25:16 +0530

    Initial commit

---
+++ src/com/Recursion/Main.java
@@ -0,0 +1,28 @@
+package com.Recursion;
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+	/*
+	  *****
+	   ****
+	    ***
+	     **
+	      *
+	 */
+        triangle(5,0);
+    }
+    public static void triangle(int row,int col){
+        if(row==0){
+            return;
+        }
+        if(col<row){
+            System.out.print("*");
+            triangle(row,col+1);
+        }
+        else{
+            System.out.println();
+            triangle(row-1,0);
+        }
+    }
+}
